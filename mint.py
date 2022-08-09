1be460a64808800c2d06fc77323ee8ad1c234bca
mteam88 <matthew8edelen@gmail.com>
2022-08-08 21:03:10 -0400

    need to check if metamask is connected

--- mint.py
+++ mint.py
@@ -10,7 +10,7 @@ w3 = Web3(Web3.HTTPProvider('https://polygon-mumbai.g.alchemy.com/v2/pq-GZIff2RI
 def generate_donate_transaction(from_address):
     return json.dumps({
         'nonce': str(w3.eth.get_transaction_count(Web3.toChecksumAddress(from_address))),
-        'from': '0x18181F285D95135F400b5710650a66C6De9aF3ce',
+        'from': from_address,
         'to': '0x18181F285D95135F400b5710650a66C6De9aF3ce',
         'value': "0x1000000000000000",
         'data': '',
--- static/index.js
+++ static/index.js
@@ -4,21 +4,31 @@ window.onerror = function(msg, url, linenumber) {
 }
 
 function connectwallet() {
-  if (typeof window.ethereum == 'undefined') {
+  if (typeof window.ethereum == undefined) {
     alert('Please install Metamask to use this app!');
   }
   ethereum.request({ method: 'eth_requestAccounts' });
 }
 
+const isMetaMaskConnected = async () => {
+  const accounts = await ethereum.request({ method: 'eth_accounts' });
+  return accounts.length > 0;
+}
+
 function donate() {
-  //fetch /donate ethereum.selectedAddress
-  fetch(`/donate/${ethereum.selectedAddress}`,{
-    method: "POST",
-  }).then(response => {return response.json()})
-    .then(txjson => {
-      ethereum.request({
-        method: 'eth_sendTransaction',
-        params: [txjson],
-      });
-    })
+  alert(isMetaMaskConnected())
+  if (isMetaMaskConnected() == true) {
+    console.log("wallet not connected")
+    connectwallet()
+  } else {
+    fetch(`/donate/${ethereum.selectedAddress}`,{
+      method: "POST",
+    }).then(response => {return response.json()})
+      .then(txjson => {
+        ethereum.request({
+          method: 'eth_sendTransaction',
+          params: [txjson],
+        }).catch(error => {alert(JSON.stringify(error.message))});
+      })
+  }
 }
